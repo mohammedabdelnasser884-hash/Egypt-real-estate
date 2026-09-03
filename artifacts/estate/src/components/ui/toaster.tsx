a9a50dfaec2ab4c60b285c49505f5cce71405515
mohammedabdelnasser884-hash <mohammedabdelnasser884@gmail.com>
2026-09-03 17:38:10 +0300

    Add artifacts/estate/src/components/ui/toaster.tsx

---
+++ artifacts/estate/src/components/ui/toaster.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import * as React from "react"
+import { useToast as useToastHook } from "@/hooks/use-toast"
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastTitle,
+} from "@/components/ui/toast"
+
+export function Toaster() {
+  const { toasts } = useToastHook()
+
+  return (
+    <div className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]">
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+    </div>
+  )
+}
